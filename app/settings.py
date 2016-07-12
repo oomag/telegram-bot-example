229101bdb63a132dea875e85e927260e06b9c319
Lydia Khashina <adatheleader@gmail.com>
2016-07-12 17:39:55 +0300

    edited token and url

--- app/settings.py
+++ app/settings.py
@@ -1,5 +1,5 @@
 import telegram
 
-BOT_TOKEN = '218474835:AAEfWCZLsbCIGQtZWRUjru_o52T_nkG-q6g'
-WEBHOOK_URL = 'https://etlgr.com/HOOK'
+BOT_TOKEN = '268737221:AAGdNFIz7Bi6lbXx2IZUMIosQnU9_fo77Ug'
+WEBHOOK_URL = 'https://mytelegrambot.dokkurapp.com/HOOK'
 bot = telegram.Bot(token=BOT_TOKEN)
