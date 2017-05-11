5a79f56525cf3fa570266dee01f348e704534713
Vikxaker <wolfis_rain@mail.ru>
2017-05-11 18:23:25 +0300

    1

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
-import falcon
-from resources import HookResource, SetWebhookResource, MainResource
+import telebot
 
-api = falcon.API()
-api.add_route('/', MainResource())
-api.add_route('/HOOK', HookResource())
-api.add_route('/set_webhook', SetWebhookResource())
+BOT_TOKEN = '374526741:AAGzUUxN2t9SkMXLHeA2YXuRsQf78cYGyhk'
+WEBHOOK_URL = 'https://mybots1.dokkurapp.com/HOOK'
+bot = telebot.TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
