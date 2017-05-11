2b9b2b6fa2cc5757bf200305a2c77efae2f5f2a2
Vikxaker <wolfis_rain@mail.ru>
2017-05-11 18:12:58 +0300

    1

--- app/classes/chat.py
+++ app/classes/chat.py
@@ -8,6 +8,6 @@ class Chat(object):
         self.chat_id = chat_id
         self.text = text
 
-    # main process method
-    def process(self):
-        bot.sendMessage(chat_id=self.chat_id, text=u"> {}".format(self.text))
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
