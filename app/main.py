608453016012c9e6938a252be3934be06649677b
Vikxaker <wolfis_rain@mail.ru>
2017-05-11 18:26:11 +0300

    1

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+def repeat_all_messages(message): 
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
