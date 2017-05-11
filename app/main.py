8ed126d2034203e18e631816c82692d72432cdb3
Vikxaker <wolfis_rain@mail.ru>
2017-05-11 19:01:18 +0300

    1

--- app/main.py
+++ app/main.py
@@ -7,6 +7,10 @@ WEBHOOK_PORT = 443
 WEBHOOK_LISTEN = '0.0.0.0'
 
 
+WEBHOOK_SSL_CERT = './webhook_cert.pem'
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'
+
+
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % (config.BOT_TOKEN)
@@ -40,11 +44,14 @@ bot.remove_webhook()
 
 
 bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-               )
+                certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
 cherrypy.config.update({
     'server.socket_host': WEBHOOK_LISTEN,
     'server.socket_port': WEBHOOK_PORT,
+    'server.ssl_module': 'builtin',
+    'server.ssl_certificate': WEBHOOK_SSL_CERT,
+    'server.ssl_private_key': WEBHOOK_SSL_PRIV
 })
 
 
