5b601370c3857e39ba32a672a124c9d127f27f02
Vikxaker <wolfis_rain@mail.ru>
2017-05-12 16:52:54 +0300

    1

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://myboot1.herokuapp.com//bot")
+    bot.set_webhook(url="https://myboot1.herokuapp.com/app")
     return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
