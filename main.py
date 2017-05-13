a94a8d85388d74841d76bc0c22f009b7a8df9ce4
Vikxaker <wolfis_rain@mail.ru>
2017-05-13 23:16:06 +0300

    1

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import telebot
 import cherrypy
 import time
@@ -5,7 +6,7 @@ import time
 token = "320545030:AAF8okRjCqGkFoylEamUGuN4V9v60StAvgQ"
 
 WEBHOOK_HOST = 'myboot1.herokuapp.com'
-WEBHOOK_PORT = 5000
+WEBHOOK_PORT = int(os.environ.get('PORT', 5000))
 WEBHOOK_LISTEN = '0.0.0.0'
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'
