e37ac61633bfd75ae811c40c3b03ef7de9261469
Vikxaker <wolfis_rain@mail.ru>
2017-05-13 23:12:19 +0300

    1

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ import time
 token = "320545030:AAF8okRjCqGkFoylEamUGuN4V9v60StAvgQ"
 
 WEBHOOK_HOST = 'myboot1.herokuapp.com'
-WEBHOOK_PORT = 5000  # 443, 80, 88 или 8443 (порт должен быть открыт!)
-WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
+WEBHOOK_PORT = 5000
+WEBHOOK_LISTEN = '0.0.0.0'
 
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
+WEBHOOK_SSL_CERT = './webhook_cert.pem'
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % (token)
 
 bot = telebot.TeleBot(token)
 
-# Наш вебхук-сервер
+
 class WebhookServer(object):
     @cherrypy.expose
     def index(self):
@@ -26,33 +26,33 @@ class WebhookServer(object):
             length = int(cherrypy.request.headers['content-length'])
             json_string = cherrypy.request.body.read(length).decode("utf-8")
             update = telebot.types.Update.de_json(json_string)
-            # Эта функция обеспечивает проверку входящего сообщения
+
             bot.process_new_updates([update])
             return ''
         else:
             raise cherrypy.HTTPError(403)
 
-# Хэндлер на все текстовые сообщения
+
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
     bot.reply_to(message, message.text)
 
-# Снимаем вебхук перед повторной установкой (избавляет от некоторых проблем)
+
 bot.remove_webhook()
 
 time.sleep(5)
 
- # Ставим заново вебхук
+
 bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
-# Указываем настройки сервера CherryPy
+
 cherrypy.config.update({
-    'server.socket_host': WEBHOOK_LISTEN,
-    'server.socket_port': WEBHOOK_PORT,
+    'server.socket_host': '0.0.0.0',
+    'server.socket_port': int(os.environ.get('PORT', 5000)),
     'server.ssl_module': 'builtin',
     'server.ssl_certificate': WEBHOOK_SSL_CERT,
     'server.ssl_private_key': WEBHOOK_SSL_PRIV
 })
 
- # Собственно, запуск!
+
 cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/bot': {}})
