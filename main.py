0f831ad5efe5a600bba05ee4355246b4bee37b89
Vikxaker <wolfis_rain@mail.ru>
2017-05-13 23:16:36 +0300

    1

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 token = "320545030:AAF8okRjCqGkFoylEamUGuN4V9v60StAvgQ"
 
-WEBHOOK_HOST = 'myboot1.herokuapp.com'
+WEBHOOK_HOST = '0.0.0.0'
 WEBHOOK_PORT = int(os.environ.get('PORT', 5000))
 WEBHOOK_LISTEN = '0.0.0.0'
 
