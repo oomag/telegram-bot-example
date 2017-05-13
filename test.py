a7799d563eeba50637423520aeb6f978c77d10b3
Vikxaker <wolfis_rain@mail.ru>
2017-05-13 20:40:19 +0300

    1

--- test.py
+++ test.py
@@ -11,9 +11,9 @@ server = Flask(__name__)
 bot = telebot.TeleBot(token)
 
 
-
-
-
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
