bc7618c1764c70c53799c56c0f69187667c5e486
Vikxaker <wolfis_rain@mail.ru>
2017-05-13 15:08:45 +0300

    1

--- app/main.py
+++ app/main.py
@@ -26,9 +26,6 @@ class WebhookServer(object):
 
 
 
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
 
 
 bot.remove_webhook()
