9f5bb095a2d6590ba84091808ad77147c464e605
Vikxaker <wolfis_rain@mail.ru>
2017-05-13 20:49:33 +0300

    1

--- test.py
+++ test.py
@@ -13,11 +13,11 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Hello, ' + message.from_user.first_name)
+    bot.send_message(message, 'Hello, ' + message.from_user.first_name)
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    bot.send_message(message, message.text)
 
 @server.route("/bot", methods=['POST'])
 def getMessage():
