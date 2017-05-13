abc6cf2463482a71a54f5a368aa2186c0ab912a8
Vikxaker <wolfis_rain@mail.ru>
2017-05-13 22:13:00 +0300

    1

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 import cherrypy
 import os
+import telebot
 
+token = "320545030:AAF8okRjCqGkFoylEamUGuN4V9v60StAvgQ"
+bot = telebot.TeleBot(token)
 class StringMaker(object):
     @cherrypy.expose
     def index(self):
-        return "Hello! How are you?"
+        token = "320545030:AAF8okRjCqGkFoylEamUGuN4V9v60StAvgQ"
+        bot = telebot.TeleBot(token)
+        @bot.message_handler(content_types=["text"])
+        def handle_text(message):
+            bot.send_message(message.from_user.id, message.text)
+
+        bot.polling(none_stop=True, interval=0)
 
 cherrypy.config.update({
     'server.socket_host': '0.0.0.0',
