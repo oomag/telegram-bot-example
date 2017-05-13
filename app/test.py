db8fefe586d0784760174c9e6627a5cd601e7c2b
Vikxaker <wolfis_rain@mail.ru>
2017-05-13 20:12:03 +0300

    1

--- app/test.py
+++ app/test.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3.6
 # -*- coding: utf-8 -*-
+import os
 import telebot
 import config
 from flask import Flask, request
 
+server = Flask(__name__)
+bot = telebot.TeleBot(config.token)
+
+
 @server.route("/bot", methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-return "!", 200
+    return "!", 200
 
 
 @server.route("/")
 def webhook():
     bot.remove_webhook()
     bot.set_webhook(config.url)
-return "!", 200
+    return "!", 200
 
 server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
-server = Flask(__name__)
 
 
 
 
 
-bot = telebot.TeleBot(config.token)
+
+
 
 
 
