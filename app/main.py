75ab364c0e6e335eb8078f57a920c64e4b6d4005
Vikxaker <wolfis_rain@mail.ru>
2017-05-13 15:11:06 +0300

    1

--- app/main.py
+++ app/main.py
@@ -9,30 +9,11 @@ bot = telebot.TeleBot(config.token)
 
 
 
-class WebhookServer(object):
-    @cherrypy.expose
-    def index(self):
-        if 'content-length' in cherrypy.request.headers and \
-                        'content-type' in cherrypy.request.headers and \
-                        cherrypy.request.headers['content-type'] == 'application/json':
-            length = int(cherrypy.request.headers['content-length'])
-            json_string = cherrypy.request.body.read(length).decode("utf-8")
-            update = telebot.types.Update.de_json(json_string)
-
-            bot.process_new_updates([update])
-            return ''
-        else:
-            raise cherrypy.HTTPError(403)
 
 
 
 
 
-bot.remove_webhook()
-
-
-bot.set_webhook(url=cherryconf.WEBHOOK_URL_BASE + cherryconf.WEBHOOK_URL_PATH,
-                certificate=open(cherryconf.WEBHOOK_SSL_CERT, 'r'))
 
 
 cherrypy.config.update({
@@ -44,4 +25,4 @@ cherrypy.config.update({
 })
 
 
-cherrypy.quickstart(WebhookServer(), cherryconf.WEBHOOK_URL_PATH, {'/app': {}})
+cherrypy.quickstart( cherryconf.WEBHOOK_URL_PATH, {'/app': {}})
