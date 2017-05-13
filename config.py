b7786a0f76a54709378be69a5e4f52ab36bd5c44
Vikxaker <wolfis_rain@mail.ru>
2017-05-13 20:22:11 +0300

    1

---
+++ config.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+
+token = '374526741:AAGzUUxN2t9SkMXLHeA2YXuRsQf78cYGyhk'
+url = 'https://myboot1.herokuapp.com/bot'
+
---
+++ test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-
+import os
+import telebot
+import config
+from flask import Flask, request
+
+server = Flask(__name__)
+bot = telebot.TeleBot(config.token)
+
+
+@server.route("/bot", methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(config.url)
+    return "!", 200
+
+
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+
+
