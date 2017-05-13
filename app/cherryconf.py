fa32165a66882aa090dbd6337c889a45b21661b0
Vikxaker <wolfis_rain@mail.ru>
2017-05-13 19:43:54 +0300

    1

--- app/cherryconf.py
+++ app/cherryconf.py
@@ -6,8 +6,8 @@ WEBHOOK_HOST = 'url'
 WEBHOOK_PORT = 5000
 WEBHOOK_LISTEN = '0.0.0.0'
 
-WEBHOOK_SSL_CERT = 'webhook_cert.pem'
-WEBHOOK_SSL_PRIV = 'webhook_pkey.pem'
+WEBHOOK_SSL_CERT = './webhook_cert.pem'
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % (config.token)
