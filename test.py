5b0ad0fb337a306b907b6986c500a5a1b22e5393
Vikxaker <wolfis_rain@mail.ru>
2017-05-13 22:39:03 +0300

    1

--- test.py
+++ test.py
@@ -3,17 +3,38 @@ import os
 import telebot
 
 token = "320545030:AAF8okRjCqGkFoylEamUGuN4V9v60StAvgQ"
+
+WEBHOOK_HOST = 'myboot1.herokuapp.com'
+WEBHOOK_PORT = 5000
+WEBHOOK_LISTEN = '0.0.0.0'
+
+WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/%s/" % (token)
+
 bot = telebot.TeleBot(token)
-class StringMaker(object):
+class WebhookServer(object):
     @cherrypy.expose
     def index(self):
-        token = "320545030:AAF8okRjCqGkFoylEamUGuN4V9v60StAvgQ"
-        bot = telebot.TeleBot(token)
-        @bot.message_handler(content_types=["text"])
-        def handle_text(message):
-            bot.send_message(message.from_user.id, message.text)
+        if 'content-length' in cherrypy.request.headers and \
+                        'content-type' in cherrypy.request.headers and \
+                        cherrypy.request.headers['content-type'] == 'application/json':
+            length = int(cherrypy.request.headers['content-length'])
+            json_string = cherrypy.request.body.read(length).decode("utf-8")
+            update = telebot.types.Update.de_json(json_string)
 
-        bot.polling(none_stop=True, interval=0)
+            bot.process_new_updates([update])
+            return ''
+        else:
+            raise cherrypy.HTTPError(403)
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+
+bot.remove_webhook()
+
+
+bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH)
 
 cherrypy.config.update({
     'server.socket_host': '0.0.0.0',
@@ -21,4 +42,4 @@ cherrypy.config.update({
 })
 
 
-cherrypy.quickstart(StringMaker())
+cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/bot': {}})
