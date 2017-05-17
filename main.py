6b5ed454ad1f6d3bcfcb7a084f49486ed50273a4
Vikxaker <wolfis_rain@mail.ru>
2017-05-17 15:35:17 +0300

    2

--- main.py
+++ main.py
@@ -9,34 +9,13 @@ WEBHOOK_HOST = '0.0.0.0'
 WEBHOOK_PORT = int(os.environ.get('PORT', 5000))
 WEBHOOK_LISTEN = '0.0.0.0'
 
-WEBHOOK_SSL_CERT = './webhook_cert.pem'
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'
-
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (token)
-
 bot = telebot.TeleBot(token)
 
 
 class WebhookServer(object):
     @cherrypy.expose
     def index(self):
-        if 'content-length' in cherrypy.request.headers and \
-                        'content-type' in cherrypy.request.headers and \
-                        cherrypy.request.headers['content-type'] == 'application/json':
-            length = int(cherrypy.request.headers['content-length'])
-            json_string = cherrypy.request.body.read(length).decode("utf-8")
-            update = telebot.types.Update.de_json(json_string)
-
-            bot.process_new_updates([update])
-            return ''
-        else:
-            raise cherrypy.HTTPError(403)
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+        return 0
 
 
 bot.remove_webhook()
@@ -44,16 +23,13 @@ bot.remove_webhook()
 time.sleep(5)
 
 
-bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,certificate=open(WEBHOOK_SSL_CERT, 'r'))
+bot.set_webhook("https://bigrussianbot.herokuapp.com/" + token)
 
 
 cherrypy.config.update({
     'server.socket_host': '0.0.0.0',
     'server.socket_port': int(os.environ.get('PORT', 5000)),
-    'server.ssl_module': 'builtin',
-    'server.ssl_certificate': WEBHOOK_SSL_CERT,
-    'server.ssl_private_key': WEBHOOK_SSL_PRIV
 })
 
 
-cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/bot': {}})
+cherrypy.quickstart(WebhookServer())
